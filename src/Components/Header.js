ec6c13cc73d29e159333c1cbd74e6ddc88347f4f
Rajnish <srajnish235.rs@gmail.com>
2023-09-07 10:33:02 +0530

    Refactring

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,9 @@
-import Search from "./Search";
 
 const Header = () => {
   return (
-    <div className="w-full flex flex-row justify-between items-center shadow-md m-2 p-2 bg-blue-950	">
-      <img
-        className="w-28 mt-1"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+   <div>
 
-    <div className="w-3/4 h-7 ml-5">
-    <Search />
-    <button>🔍</button>
-    </div>
-      <div className="w-1/2 flex">
-        <ul className=" w-full flex flex-row justify-evenly h-14 items-center p-3 font-medium text-lg text-white">
-          <li>Orders</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
+   </div>
   );
 };
 
