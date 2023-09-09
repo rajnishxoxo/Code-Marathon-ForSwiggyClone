af18d1cf83d021c4a07a29e143ce1fe022cacd08
Rajnish <srajnish235.rs@gmail.com>
2023-09-09 12:13:55 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -129,7 +129,7 @@ const Body = () => {
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
           onClick={handleRating}
         >
-          HighlyRated
+          Highly Rated Products
         </button>
       </div>
 
