139c6cb5ddec933ee6282d343f480f4ac12b7a12
Rajnish <srajnish235.rs@gmail.com>
2023-09-11 13:03:21 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,23 +3,15 @@ import { useSelector } from "react-redux";
 const Cart = () => {
   const cart = useSelector((store) => store.cart.item);
 
-  console.log(cart);
-
   return (
     <div>
       <h1 className="text-center font-semibold text-4xl m-5">Cart</h1>
 
-      <div className=" border w-2/4 h-auto">
-        <h1 className="text-lg font-light mx-4">Item List</h1>
-        {cart.map((data) => {
-          return (
-            <div>
-              <h1 className="text-lg font-normal m-3"> {data.list.title}</h1>
+      {cart.map((data) => {
+        const { title, image, price } = data.list;
 
-            </div>
-          );
-        })}
-      </div>
+        console.log(title, image, price);
+      })}
     </div>
   );
 };
