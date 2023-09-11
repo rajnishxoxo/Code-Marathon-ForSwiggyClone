e916583fe3267367649bf6022f48d590e93819d3
Rajnish <srajnish235.rs@gmail.com>
2023-09-11 15:16:01 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, removeItem } from "../Utils/cartSlice";
 
 const Cart = () => {
   const cart = useSelector((store) => store.cart.item);
 
-  console.log(cart)
+ 
 
   const dispatch = useDispatch();
 
-  const handleAddItem=(data)=>{
-
-    dispatch(addItem(data))
-
-  }
+  const handleAddItem = (data) => {
+    dispatch(addItem(data));
+  };
 
   const handleRemoveItem = (itemData) => {
     dispatch(removeItem(itemData));
   };
 
-
   return (
+    cart.length==0?<h1 className="text-center font-bold text-2xl m-5">Please Add Item To Your Cart..</h1> :(
     <div>
       {cart.map((data) => {
-        const { title, image, price,id } = data.list;
+        const { title, image, price, id } = data.list;
 
-        console.log(id)
+        console.log(id);
 
         return (
           <div key={data.id} className="flex items-center m-3">
@@ -36,21 +34,27 @@ const Cart = () => {
             />
             <p>{title}</p>
             <div className="ml-auto m-2">
-              <button className="font-medium text-2xl bg-slate-700 rounded-md mx-2 w-10" onClick={()=>{
-                handleAddItem(data)
-              }}>
+              <button
+                className="font-medium text-2xl bg-slate-700 rounded-md mx-2 w-10"
+                onClick={() => {
+                  handleAddItem(data);
+                }}
+              >
                 +
               </button>
-              <button className="font-medium text-2xl bg-slate-700 rounded-md w-10"  onClick={()=>{
-                handleRemoveItem(data)
-              }}>
+              <button
+                className="font-medium text-2xl bg-slate-700 rounded-md w-10"
+                onClick={() => {
+                  handleRemoveItem(data);
+                }}
+              >
                 -
               </button>
             </div>
           </div>
         );
       })}
-    </div>
+    </div>)
   );
 };
 
