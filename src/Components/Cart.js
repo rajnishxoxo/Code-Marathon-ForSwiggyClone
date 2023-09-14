716db05280dbf050ca0f77e8d1f8161d2fb69953
Rajnish <srajnish235.rs@gmail.com>
2023-09-14 18:58:16 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -5,7 +5,38 @@ import { addItem, removeItem } from "../Utils/cartSlice";
 const Cart = () => {
   const cart = useSelector((store) => store.cart.item);
 
+  // console.log(cart)
+
+  const itemCount ={};
+
+  cart.map((data)=>{
+    const{id} = data.list;
+    if(itemCount[id]){
+      itemCount[id]++
+    }else{
+      itemCount[id]=1;
+    }
+  })
+
+  const uniqueItem = Object.keys(itemCount).map((data)=>{
+    // console.log(data)
+    const firstOccur = cart.find((item)=>{
+      return item.list.id==data;
+    })
+
+    return{
+      item:firstOccur ,
+      count:itemCount[data]
+    }
+    
+  });
+
+  console.log(uniqueItem);
+
+
   const dispatch = useDispatch();
+
+  
 
   const handleAddItem = (data) => {
     dispatch(addItem(data));
@@ -31,20 +62,23 @@ const Cart = () => {
               alt={title}
             />
             <p>{title}</p>
-            <div className="ml-auto m-2">
+            <div className="ml-auto m-2 flex flex-row">
               <button
                 className="font-medium text-2xl bg-slate-700 rounded-md mx-2 w-10"
-                onClick={() => {
-                  handleAddItem(data);
-                }}
+                // onClick={() => {
+                //   handleAddItem(data);
+                // }}
               >
                 +
               </button>
+              <div className="text-center text-lg font-bold">
+                {/* {data.count} */}
+              </div>
               <button
                 className="font-medium text-2xl bg-slate-700 rounded-md w-10"
-                onClick={() => {
-                  handleRemoveItem(data);
-                }}
+                // onClick={() => {
+                //   handleRemoveItem(data);
+                // }}
               >
                 -
               </button>
