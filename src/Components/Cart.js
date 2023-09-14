df046460daec4989442ceb30d1e5b212c08b65b3
Rajnish <srajnish235.rs@gmail.com>
2023-09-14 21:35:19 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -7,36 +7,32 @@ const Cart = () => {
 
   // console.log(cart)
 
-  const itemCount ={};
+  const itemCount = {};
 
-  cart.map((data)=>{
-    const{id} = data.list;
-    if(itemCount[id]){
-      itemCount[id]++
-    }else{
-      itemCount[id]=1;
+  cart.map((data) => {
+    const { id } = data?.list;
+    if (itemCount[id]) {
+      itemCount[id]++;
+    } else {
+      itemCount[id] = 1;
     }
-  })
-
-  const uniqueItem = Object.keys(itemCount).map((data)=>{
-    // console.log(data)
-    const firstOccur = cart.find((item)=>{
-      return item.list.id==data;
-    })
-
-    return{
-      item:firstOccur ,
-      count:itemCount[data]
-    }
-    
   });
 
-  console.log(uniqueItem);
+  const uniqueItem = Object.keys(itemCount).map((data) => {
+    // console.log(data)
+    const firstOccur = cart.find((item) => {
+      return item.list.id == data;
+    });
 
+    return {
+      item: firstOccur,
+      count: itemCount[data],
+    };
+  });
+
+  // console.log(uniqueItem);
 
   const dispatch = useDispatch();
-
-  
 
   const handleAddItem = (data) => {
     dispatch(addItem(data));
@@ -51,11 +47,12 @@ const Cart = () => {
       Please Add Item To Your Cart..
     </h1>
   ) : (
-    <div>
-      {cart.map((data) => {
-        const { title, image, price, id } = data.list;
+    <div className=" flex flex-row items-center">
+      {uniqueItem.map((data) => {
+        const { title, image, price, id } = data.item.list;
         return (
-          <div key={data.id} className="flex items-center m-3">
+          
+          <div key={data.id} className="flex items-center w-1/2 m-3 border">
             <img
               className="w-40 h-20 object-contain m-3"
               src={image}
@@ -71,8 +68,8 @@ const Cart = () => {
               >
                 +
               </button>
-              <div className="text-center text-lg font-bold">
-                {/* {data.count} */}
+              <div className="text-center m-2 items-center text-lg font-bold">
+                {data.count}
               </div>
               <button
                 className="font-medium text-2xl bg-slate-700 rounded-md w-10"
@@ -84,8 +81,32 @@ const Cart = () => {
               </button>
             </div>
           </div>
+          
         );
       })}
+
+      <div>
+        {uniqueItem && (
+          <div className="w-full border h-2/3 flex items-center justify-between p-2">
+            
+            <div className="flex items-center flex-col">
+              <h1 className="text-lg font-medium">Order total:</h1>
+              <div className="ml-1">
+                <h1 className="text-lg font-normal">
+                  Product{" "}
+                  <span className="text-lg font-normal ml-1">
+                    {cart.length}
+                  </span>
+                  <span>
+              Total Price: $
+              {uniqueItem.reduce((acc, data) => acc + data.item.list.price, 0)}
+            </span>
+                </h1>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
