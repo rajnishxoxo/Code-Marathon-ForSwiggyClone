5ead0923cea6cbc36d92b0aeb47fdde88985fc23
Rajnish <srajnish235.rs@gmail.com>
2023-09-15 17:28:52 +0530

    Update cartSlice.js

--- src/Utils/cartSlice.js
+++ src/Utils/cartSlice.js
@@ -13,8 +13,17 @@ const cartSlice = createSlice({
         },
 
         removeItem: (state, action) => {
-            const itemIdToRemove = action.payload.list.id; // Access the ID from the list property
-            state.item = state.item.filter((item) => item.list.id !== itemIdToRemove);
+          const itemToDeleted = action.payload;
+
+
+          const indexToDelete = state.item.findIndex((data)=>{
+           return data.list.id===itemToDeleted.list.id
+          })
+
+          if(indexToDelete!=-1){
+            state.item.splice(indexToDelete,1)
+          }
+
           },
         //   With these changes, you should pass the entire item object you want to remove to the action, and the reducer will correctly remove only the item with the matching ID while keeping the others in the cart.
           
