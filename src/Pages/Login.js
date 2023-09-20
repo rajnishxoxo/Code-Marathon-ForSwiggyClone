5428af1ff38c98ca8c4f25298091a7c8c134eb9e
Rajnish <srajnish235.rs@gmail.com>
2023-09-20 13:25:48 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,48 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const Login = () => {
+  const [signUp, setSignUp] = useState(false);
+  const handleToggle = ()=>{
+    if(signUp===true){
+      setSignUp(false)
+    }else{
+      setSignUp(true)
+    }
+  }
   return (
     <>
-     
-      <div >
-        <h1 className="text-center m-4 p-4 text-4xl font-semibold border-b-2">Login</h1>
-        
-        <div >
-          <div className="w-full m-auto" >
+      <div>
+        <h1 className="text-center m-4 p-4 text-4xl font-semibold border-b-2">
+          Login
+        </h1>
+
+        <div>
+          <div className="w-1/2 m-auto">
             <form className="m-auto">
-              <div   className="w-full m-auto">
-                <label for="display-4">Email address</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                />
-              </div>
-              <div>
-                <label for="floatingPassword display-4">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="floatingPassword"
-                  placeholder="Password"
-                />
-              </div>
-              <div>
-                <p>New Here?Register</p>
-              </div>
-              <div >
-                <button type="submit" disabled>
-                  Login
-                </button>
-              </div>
+              {signUp&&
+               <input
+               type="text"
+               placeholder="Full Name"
+               className="w-full mb-4 p-3 rounded bg-gray-600 text-white"
+             />}
+             
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full mb-4 p-3 rounded bg-gray-600 text-white"
+              />
+
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full mb-4 p-3 rounded  bg-gray-600 text-white"
+              />
+
+             {
+               signUp?<button
+               onClick={(e) => {
+                 e.preventDefault();
+               }}
+               className="bg-red-700 w-full h-10 hover:bg-red-600 text-white font-bold rounded"
+             >
+               Sign-Up
+             </button>:
+              <button
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+              className="bg-red-700 w-full h-10 hover:bg-red-600 text-white font-bold rounded"
+            >
+              Log in
+            </button>
+
+
+             }
+              {
+                signUp?
+                <p
+                  className="text-red-700 font-bold text-2xl mb-4 cursor-pointer mt-5"
+                  onClick={handleToggle}
+                >
+                  Existing User ? Login Now
+                </p>
+              :<p
+              className="text-red-700 font-bold text-2xl mb-4 cursor-pointer mt-5"
+              onClick={handleToggle}
+            >
+              New user ? Sign up now.
+            </p>}
             </form>
           </div>
         </div>
       </div>
-   
     </>
   );
 };
